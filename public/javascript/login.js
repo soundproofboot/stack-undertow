878b1fba7093d0647ef6ac4d8a12bec031730705
C Bares <colinedwinbares@gmail.com>
2022-02-24 13:07:17 -0600

    set up login, buggy

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -15,8 +15,30 @@ async function signUp(e){
       }),
       headers: { 'Content-Type': 'application/json' }
     });
-    if (!response.ok) console.log(response.statusText);
+    if (!response.ok) console.log('success');
+    else console.log(response.statusText);
   }
 };
 
+async function login(e) {
+  e.preventDefault();
+
+  const email = document.querySelector('#email-login').value.trim();
+  const password = document.querySelector('#password-login').value.trim();
+
+  if (email && password) {
+    const response = await fetch('/api/users/login', {
+      method: 'POST',
+      body: JSON.stringify({
+        email,
+        password
+      }),
+      headers: { 'Content-Type': 'application/json' }
+    });
+    if (response.ok) document.location.replace('/dashboard');
+    else console.log(response.statusText);
+  }
+}
+
 document.querySelector('.signup-form').addEventListener('submit', signUp);
+document.querySelector('.login-form').addEventListener('submit', login);
