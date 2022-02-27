4cdf4053ea98db21271c77fb3da13c50f3b6e20b
C Bares <colinedwinbares@gmail.com>
2022-02-27 16:19:19 -0600

    split log in/sign up and all functionality to separate views

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,29 +1,4 @@
-async function signUp(e){
-  e.preventDefault();
 
-  const username = document.querySelector('#username-signup').value.trim();
-  const email = document.querySelector('#email-signup').value.trim();
-  const password = document.querySelector('#password-signup').value.trim();
-
-  if (username && email && password) {
-    const response = await fetch('/api/users', {
-      method: 'POST',
-      body: JSON.stringify({
-        username,
-        email,
-        password
-      }),
-      headers: { 'Content-Type': 'application/json' }
-    });
-    
-    if (response.ok) {
-      console.log('success');
-      document.location.replace('/dashboard');
-      
-    }
-    else console.log(response.statusText);
-  }
-};
 
 async function login(e) {
   e.preventDefault();
@@ -45,5 +20,4 @@ async function login(e) {
   }
 }
 
-document.querySelector('.signup-form').addEventListener('submit', signUp);
 document.querySelector('.login-form').addEventListener('submit', login);
